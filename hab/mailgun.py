0ad71b23205bb0cf5e63d2cc0ad035184ee41300
William Mayor <mail@williammayor.co.uk>
2015-06-06 12:47:45 +0100

    Error catching emails, BCC the couple too.

--- hab/mailgun.py
+++ hab/mailgun.py
@@ -4,21 +4,28 @@ import requests
 
 def send_emails(form):
     if form.email.data:
+        try:
+            requests.post(
+                '%s/messages' % current_app.config['MAILGUN_URL'],
+                auth=('api', current_app.config['MAILGUN_API_KEY']),
+                data={
+                    'from': 'Helen and Billy <thecouple@helenandbilly.co.uk>',
+                    'bcc': 'thecouple@helenandbilly.co.uk',
+                    'to': form.email.data,
+                    'subject': 'RSVP Confirmation',
+                    'text': render_template(
+                        'emails/rsvp_confirmation.txt', form=form)})
+        except:
+            pass
+    try:
         requests.post(
             '%s/messages' % current_app.config['MAILGUN_URL'],
             auth=('api', current_app.config['MAILGUN_API_KEY']),
             data={
-                'from': 'Helen and Billy <thecouple@helenandbilly.co.uk>',
-                'to': form.email.data,
-                'subject': 'RSVP Confirmation',
+                'from': 'RSVP Bot <rsvpbot@helenandbilly.co.uk>',
+                'to': 'thecouple@helenandbilly.co.uk',
+                'subject': 'You Got an RSVP',
                 'text': render_template(
-                    'emails/rsvp_confirmation.txt', form=form)})
-    requests.post(
-        '%s/messages' % current_app.config['MAILGUN_URL'],
-        auth=('api', current_app.config['MAILGUN_API_KEY']),
-        data={
-            'from': 'RSVP Bot <rsvpbot@helenandbilly.co.uk>',
-            'to': 'thecouple@helenandbilly.co.uk',
-            'subject': 'You Got an RSVP',
-            'text': render_template(
-                'emails/rsvp_forward.txt', form=form)})
+                    'emails/rsvp_forward.txt', form=form)})
+    except:
+        pass
